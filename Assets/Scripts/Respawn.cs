75ae62577f4a1c465084e4e4940958a8e3c3678c
TheFileExplorer <126206720+TheFileExplorer@users.noreply.github.com>
2024-03-06 14:14:11 +1100

    First Commit

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+public float threshold;
+
+
+    void FixedUpdate()
+    {
+        if (transform.position.y < threshold)
+        {
+              transform.position = new Vector3(0f, 0.5f, 0f );
+        }
+    }
+}
+
+
